581f8173a603940d8cb94206d6349b189ebfcc78
kaysaith1900 <kaysaith@qq.com>
2020-10-31 13:27:55 +0800

    perf(util): remove color util

--- app/util/common.ts
+++ app/util/common.ts
@@ -7,19 +7,6 @@ export const CommonUtil = {
   getRandomId: (prefix?: string) => {
     return ((prefix || '') + Math.random().toString(16).substr(2)).substring(0, 12)
   },
-  hexToRgba: (hex: string, alpha = 1): string => {
-    let color: any;
-    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-      color = hex.substring(1).split('');
-      if (color.length === 3) {
-        color = [color[0], color[0], color[1], color[1], color[2], color[2]];
-      }
-      color = `0x${color.join('')}`;
-
-      return `rgba(${[(color >> 16) & 255, (color >> 8) & 255, color & 255].join(',')},${alpha})`;
-    }
-    throw new Error('Bad Hex');
-  },
   pickNumber: (content: string): number | undefined => {
     const result = content.replace(/[^0-9]/ig, '');
     return result ? parseInt(result) : undefined;
