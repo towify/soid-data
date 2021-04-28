76a5cf2cafa92b9f15728c11982fd67271cd2a72
sunxingxiang <linghuchong.sun@towify.com>
2021-04-28 11:26:06 +0800

    feat(array): 为 array 的工具类增加一个包装类

--- app/util/array.util.ts
+++ app/util/array.util.ts
@@ -3,12 +3,23 @@
  * @date 12/23/20 11:10
  */
 
-export function flat<U>(array: U[][]): U[] {
-  if ((array as unknown as U[]).every((value) => !Array.isArray(value))) {
-    return (array as unknown as U[]).slice();
+export class ArrayUtils {
+  /**
+   * @description: 将二维数组降为一维数组
+   * @param {U} array
+   */
+  static flat<U>(array: U[][]): U[] {
+    if (((array as unknown) as U[]).every((value) => !Array.isArray(value))) {
+      return ((array as unknown) as U[]).slice();
+    }
+    return array.reduce(
+      (acc, value) =>
+        acc.concat(
+          Array.isArray(value)
+            ? ArrayUtils.flat((value as unknown) as U[][])
+            : value
+        ),
+      []
+    );
   }
-  return array
-    .reduce((acc, value) => acc
-      .concat(Array.isArray(value) ? flat((value as unknown as U[][])) : value), []);
 }
-
