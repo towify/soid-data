7f240c60c761035c12f69592a7c1ae84fb0102c7
kaysaith1900 <kaysaith@qq.com>
2021-04-28 17:27:42 +0800

    feat(Test): add jest test module

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/**
+ *  @Author KaySaith
+ *  @Date 11/12/20
+ */
+
+
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    roots: ["./app"],
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    transformIgnorePatterns: [`/node_modules/(?!soid-data|towify-scf-client-engine|towify-scf-values)`],
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    globals: {
+        "ts-jest": {
+            tsconfig: {
+                allowJs: true,
+            },
+        },
+    },
+};
