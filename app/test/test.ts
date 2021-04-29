0f806272c2bcf867927375dc0216380265db1734
sunxingxiang <linghuchong.sun@towify.com>
2021-04-29 13:12:44 +0800

    feat(performance): 增加 Performance 对外导出类，并增加方法注释

--- app/test/test.ts
+++ app/test/test.ts
@@ -4,10 +4,42 @@
  */
 
 import {Shared} from "../storage/shared_preference";
+import {Performance} from "../util/performance";
 
 describe('soid-data', () => {
-    it('getSharedPreference', async () => {
-        await Shared.save('some_data', 'some data');
-        expect(Shared.get('some_data') === 'some data');
-    });
+  it('getSharedPreference', async () => {
+    await Shared.save('some_data', 'some data');
+    expect(Shared.get('some_data') === 'some data');
+  });
+
+  it('performance-debounce', () => {
+    let count = 0;
+    const debounceFunction = Performance.debounce(() => {
+      count += 1;
+    }, 200);
+
+    jest.useFakeTimers();
+    setTimeout(debounceFunction, 0);
+    setTimeout(debounceFunction, 100);
+    setTimeout(debounceFunction, 300);
+    jest.runAllTimers();
+    expect(count).toEqual(2);
+  });
+
+  it('performance-throttle', () => {
+    let count = 0;
+    const throttleFunction = Performance.throttle(() => {
+      count += 1;
+    }, 200);
+
+    jest.useFakeTimers();
+    throttleFunction()
+    throttleFunction()
+    throttleFunction()
+    throttleFunction()
+    jest.runAllTimers();
+
+    expect(count === 4);
+  });
+
 });
--- app/util/performance.ts
+++ app/util/performance.ts
@@ -3,41 +3,59 @@
  * @date 2020/10/6 15:20
  */
 
-export function debounce<T extends any[]>(
-  action: (...args: T) => any,
-  timeout: number,
-): (...args: T) => void {
-  let timer: number;
-  return (...args: T) => {
-    window.clearTimeout(timer);
-    timer = window.setTimeout(() => action(...args), timeout);
-  };
-}
+export class Performance {
 
-export function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export function throttle<T extends (...args: any[]) => any>(
-  func: T,
-  threshhold: number = 20,
-  scope?: any
-): T {
-  if (threshhold < 0) {
-    throw new TypeError("invalid thresh hold value.");
+  /**
+   * @description 防抖动
+   * @param action 回调方法
+   * @param timeout 时间长度，单位ms
+   */
+  static debounce<T extends any[]>(
+    action: (...args: T) => any,
+    timeout: number,
+  ): (...args: T) => void {
+    let timer: number;
+    return (...args: T) => {
+      window.clearTimeout(timer);
+      timer = window.setTimeout(() => action(...args), timeout);
+    };
   }
 
-  let timer: number = 0;
-  let rtn: any;
+  /**
+   * @description 延迟调用
+   * @param  ms 延迟时间，单位ms
+   */
+  static delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-  return <T>function (this: any, ...args: any[]): any {
-    if (!timer) {
-      const ctx = typeof scope === "undefined" ? this : scope;
-      timer = window.setTimeout(() => {
-        timer = 0;
-        rtn = func.apply(ctx, args);
-      }, threshhold);
+  /**
+   * @description 间隔调用方法
+   * @param func 方法
+   * @param threshhold 间隔时间，单位ms
+   * @param scope 方法调用者
+   */
+  static throttle<T extends (...args: any[]) => any>(
+    func: T,
+    threshhold: number = 20,
+    scope?: any
+  ): T {
+    if (threshhold < 0) {
+      throw new TypeError("invalid thresh hold value.");
     }
-    return rtn;
-  };
+
+    let timer: number = 0;
+    let rtn: any;
+
+    return <T>function (this: any, ...args: any[]): any {
+      if (!timer) {
+        const ctx = typeof scope === "undefined" ? this : scope;
+        timer = window.setTimeout(() => {
+          timer = 0;
+          rtn = func.apply(ctx, args);
+        }, threshhold);
+      }
+      return rtn;
+    };
+  }
 }
