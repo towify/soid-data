44dfc6c5c131151f81398bc7ba6fd1d08957e8bc
kaysaith <kaysaith@qq.com>
2022-07-05 15:34:07 +0800

    perf(Object): deep optional set

--- app/util/object.util.ts
+++ app/util/object.util.ts
@@ -23,7 +23,7 @@ export class ObjectUtils {
       return ObjectUtils.deepSet(object[keyPaths[0]], keyPaths.slice(1), value);
   }
 
-  static deepGet(source: {[key: string]: any}, keyPath: string[]) {
+  static deepGet(source: { [key: string]: any }, keyPath: string[]) {
     const clonedPath = [...keyPath];
     let result = source;
     if (!source[clonedPath[0]]) clonedPath.splice(0, 1);
@@ -32,5 +32,62 @@ export class ObjectUtils {
       clonedPath.splice(0, 1);
     }
     return result;
+  }
+
+  /**
+   * 如果设置的 Path 深度没有结束，其中某个节点为 undefined 的话那么会生成一个 空的 Object
+   * */
+  static deepOptionalSet(params: {
+    path: string[];
+    source: any;
+    value: any;
+    onBindDifferentValue?: () => void;
+  }): void {
+    const setDeepValueFunction = (deepPath: string[], deepSource: any, deepValue: any) => {
+      if (deepPath.length && deepPath.length === 1 && deepSource[deepPath[0]] === undefined) {
+        deepSource[deepPath[0]] = deepValue;
+        params.onBindDifferentValue && params.onBindDifferentValue();
+        return;
+      }
+      if (deepPath.length > 1) {
+        if (!deepSource[deepPath[0]]) {
+          deepSource[deepPath[0]] = {};
+        }
+        setDeepValueFunction(
+          deepPath.slice(1, deepPath.length),
+          deepSource[deepPath[0]],
+          deepValue
+        );
+      } else if (deepPath.length) {
+        let isDifferent = false;
+        if (typeof deepSource[deepPath[0]] === typeof deepValue) {
+          isDifferent = deepSource[deepPath[0]] !== deepValue;
+          deepSource[deepPath[0]] = deepValue;
+          if (isDifferent) params.onBindDifferentValue && params.onBindDifferentValue();
+        } else if (typeof deepSource[deepPath[0]] === 'string') {
+          if (deepSource[deepPath[0]] !== `${ deepValue }`) {
+            params.onBindDifferentValue && params.onBindDifferentValue();
+          }
+          deepSource[deepPath[0]] = `${ deepValue }`;
+        } else if (typeof deepSource[deepPath[0]] === 'number') {
+          // todo
+        } else if (typeof deepSource[deepPath[0]] === 'string') {
+          isDifferent = deepSource[deepPath[0]] !== parseFloat(deepValue);
+          const formatted = parseFloat(deepValue);
+          if (Number.isNaN(formatted)) {
+            console.error(
+              `Towify: Invalid Deep Value Source, it should be number but it is ${ deepValue }`
+            );
+          }
+          deepSource[deepPath[0]] = formatted;
+          if (isDifferent) params.onBindDifferentValue && params.onBindDifferentValue();
+        } else if (typeof deepSource[deepPath[0]] === 'boolean') {
+          isDifferent = deepSource[deepPath[0]] !== (deepValue === 'true');
+          deepSource[deepPath[0]] = deepValue === 'true';
+          if (isDifferent) params.onBindDifferentValue && params.onBindDifferentValue();
+        }
+      }
+    };
+    setDeepValueFunction(params.path, params.source, params.value);
   }
 }
