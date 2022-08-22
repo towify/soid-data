99f2c7961da8b8a67ca84e4f091aac1144f83c15
allen <allen.zhang@towify.com>
2022-08-22 18:36:28 +0800

    fixed(date): weekday format

--- app/util/date.util.ts
+++ app/util/date.util.ts
@@ -88,19 +88,19 @@ export class DateUtil {
   static formatWeekDay(day: number): number | string {
     switch (day) {
       case 0:
-        return '星期天'
+        return 'Sunday'
       case 1:
-        return '星期一';
+        return 'Monday';
       case 2:
-        return '星期二';
+        return 'Tuesday';
       case 3:
-        return '星期三';
+        return 'Wednesday';
       case 4:
-        return '星期四';
+        return 'Thursday';
       case 5:
-        return '星期五';
+        return 'Friday';
       case 6:
-        return '星期六';
+        return 'Saturday';
       default:
         break;
     }
