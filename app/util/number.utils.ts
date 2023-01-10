fd9af2230b338946300f8434733f929777776364
kaysaith <kaysaith@qq.com>
2023-01-10 23:34:40 +0800

    perf(Number): random

--- app/util/number.utils.ts
+++ app/util/number.utils.ts
@@ -8,6 +8,6 @@ export class NumberUtils {
   static getRandomIntInRange(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
